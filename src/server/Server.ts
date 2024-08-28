217aea402e3d6eb99a35da784e3ddd94010d73f3
Davi Fernandes <davifncosta@gmail.com>
2024-08-28 20:27:20 -0300

    feat: add folders structure

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import { router } from '../routes';
+import "dotenv/config"
 
 const server = express();
+
+server.use(express.json());
+
+server.use("api", router)
 
 server.get('/', (req, res) => {
   return res.send('Hello World!');
