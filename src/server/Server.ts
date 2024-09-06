1d010f2143817a18a63a7c01c32ac7ee4210527a
Davi Fernandes <davifncosta@gmail.com>
2024-09-05 23:51:22 -0300

    feat: add cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from '../routes';
 import "dotenv/config"
 
 const server = express();
+
+server.use(cors())
 
 server.use(express.json());
 
